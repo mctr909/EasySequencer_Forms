8cad8a38cfe54ad9c69cbc77aca6e476dca10828
mctr909 <tr-909@hotmail.co.jp>
2020-02-17 15:20:14 +0900

    Add files via upload

---
+++ Forms/Common/CommonCtrl.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Forms {
+    public class CommonCtrl {
+        private Form mForm;
+
+        private PictureBox mBtnClose = new PictureBox();
+        private PictureBox mBtnMaximize = new PictureBox();
+        private PictureBox mBtnMinimize = new PictureBox();
+
+        private bool mMoving = false;
+        private Point mCurPos;
+
+        public int FormCtrlLeft { get; private set; }
+        public int FormCtrlBottom { get; private set; }
+
+        public EventHandler WindowStateChanged;
+
+        public CommonCtrl(Form form, bool enableMaximize = false) {
+            mForm = form;
+
+            ((System.ComponentModel.ISupportInitialize)mBtnClose).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)mBtnMaximize).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)mBtnMinimize).BeginInit();
+
+            //
+            // btnClose
+            //
+            mBtnClose.BackColor = Color.Transparent;
+            mBtnClose.Image = Properties.Resources.close_leave;
+            mBtnClose.Location = new Point(237, 4);
+            mBtnClose.Name = "btnClose";
+            mBtnClose.Size = new Size(30, 30);
+            mBtnClose.TabIndex = 7;
+            mBtnClose.TabStop = false;
+            mBtnClose.Click += new EventHandler((object s, EventArgs e) => {
+                mForm.Close();
+            });
+            mBtnClose.MouseEnter += new EventHandler((object s, EventArgs e) => {
+                if (null != mBtnClose.Image) {
+                    mBtnClose.Image.Dispose();
+                    mBtnClose.Image = null;
+                }
+                mBtnClose.Image = Properties.Resources.close_hover;
+            });
+            mBtnClose.MouseLeave += new EventHandler((object s, EventArgs e) => {
+                if (null != mBtnClose.Image) {
+                    mBtnClose.Image.Dispose();
+                    mBtnClose.Image = null;
+                }
+                mBtnClose.Image = Properties.Resources.close_leave;
+            });
+            //
+            // btnMaximize
+            //
+            mBtnMaximize.BackColor = Color.Transparent;
+            mBtnMaximize.Image = Properties.Resources.maximize_leave;
+            mBtnMaximize.Location = new Point(201, 4);
+            mBtnMaximize.Name = "btnMaximize";
+            mBtnMaximize.Size = new Size(30, 30);
+            mBtnMaximize.TabIndex = 10;
+            mBtnMaximize.TabStop = false;
+            mBtnMaximize.Click += new EventHandler((object s, EventArgs e) => {
+                if (FormWindowState.Maximized == mForm.WindowState) {
+                    mForm.WindowState = FormWindowState.Normal;
+                } else {
+                    mForm.WindowState = FormWindowState.Maximized;
+                }
+                if (null != WindowStateChanged) {
+                    WindowStateChanged.Invoke(s, e);
+                }
+            });
+            mBtnMaximize.MouseEnter += new EventHandler((object s, EventArgs e) => {
+                if (null != mBtnMaximize.Image) {
+                    mBtnMaximize.Image.Dispose();
+                    mBtnMaximize.Image = null;
+                }
+                mBtnMaximize.Image = Properties.Resources.maximize_hover;
+            });
+            mBtnMaximize.MouseLeave += new EventHandler((object s, EventArgs e) => {
+                if (null != mBtnMaximize.Image) {
+                    mBtnMaximize.Image.Dispose();
+                    mBtnMaximize.Image = null;
+                }
+                mBtnMaximize.Image = Properties.Resources.maximize_leave;
+            });
+            //
+            // btnMinimize
+            //
+            mBtnMinimize.BackColor = Color.Transparent;
+            mBtnMinimize.Image = Properties.Resources.minimize_leave;
+            mBtnMinimize.Location = new Point(201, 4);
+            mBtnMinimize.Name = "btnMinimize";
+            mBtnMinimize.Size = new Size(30, 30);
+            mBtnMinimize.TabIndex = 10;
+            mBtnMinimize.TabStop = false;
+            mBtnMinimize.Click += new EventHandler((object s, EventArgs e) => {
+                mForm.WindowState = FormWindowState.Minimized;
+                if (null != WindowStateChanged) {
+                    WindowStateChanged.Invoke(s, e);
+                }
+            });
+            mBtnMinimize.MouseEnter += new EventHandler((object s, EventArgs e) => {
+                if (null != mBtnMinimize.Image) {
+                    mBtnMinimize.Image.Dispose();
+                    mBtnMinimize.Image = null;
+                }
+                mBtnMinimize.Image = Properties.Resources.minimize_hover;
+            });
+            mBtnMinimize.MouseLeave += new EventHandler((object s, EventArgs e) => {
+                if (null != mBtnMinimize.Image) {
+                    mBtnMinimize.Image.Dispose();
+                    mBtnMinimize.Image = null;
+                }
+                mBtnMinimize.Image = Properties.Resources.minimize_leave;
+            });
+            //
+            // form
+            //
+            mForm.BackColor = Colors.FormColor;
+            mForm.MouseDown += new MouseEventHandler((object s, MouseEventArgs e) => {
+                mMoving = true;
+                mCurPos = Cursor.Position;
+            });
+            mForm.MouseUp += new MouseEventHandler((object s, MouseEventArgs e) => {
+                mMoving = false;
+            });
+            mForm.MouseMove += new MouseEventHandler((object s, MouseEventArgs e) => {
+                if (mMoving) {
+                    var screen = Screen.FromControl(mForm);
+                    var sw = screen.Bounds.Width;
+                    var sh = screen.Bounds.Height;
+                    var dx = Cursor.Position.X - mCurPos.X;
+                    var dy = Cursor.Position.Y - mCurPos.Y;
+                    var left = mForm.Left + dx;
+                    var top = mForm.Top + dy;
+                    if (left < 96 - mForm.Width) {
+                        left = 96 - mForm.Width;
+                    }
+                    if (sw - 96 < left) {
+                        left = sw - 96;
+                    }
+                    if (top < 64 - mForm.Height) {
+                        top = 64 - mForm.Height;
+                    }
+                    if (sh - 64 < top) {
+                        top = sh - 64;
+                    }
+                    mForm.Left = left;
+                    mForm.Top = top;
+                    mCurPos = Cursor.Position;
+                }
+            });
+            mForm.SizeChanged += new EventHandler((object s, EventArgs e) => {
+                mBtnClose.Top = 0;
+                mBtnClose.Left = mForm.Width - mBtnClose.Width;
+                mBtnMaximize.Top = 0;
+                mBtnMinimize.Top = 0;
+                if (enableMaximize) {
+                    mBtnMaximize.Left = mBtnClose.Left - mBtnMaximize.Width - 4;
+                    mBtnMinimize.Left = mBtnMaximize.Left - mBtnMinimize.Width - 4;
+                } else {
+                    mBtnMinimize.Left = mBtnClose.Left - mBtnMinimize.Width - 4;
+                }
+                FormCtrlLeft = mBtnMinimize.Left;
+                FormCtrlBottom = mBtnClose.Bottom;
+            });
+            mForm.Controls.Add(mBtnClose);
+            if (enableMaximize) {
+                mForm.Controls.Add(mBtnMaximize);
+            }
+            mForm.Controls.Add(mBtnMinimize);
+
+            ((System.ComponentModel.ISupportInitialize)mBtnClose).EndInit();
+            ((System.ComponentModel.ISupportInitialize)mBtnMaximize).EndInit();
+            ((System.ComponentModel.ISupportInitialize)mBtnMinimize).EndInit();
+        }
+    }
+}
---
+++ Forms/Comp.cs
@@ -0,0 +1,300 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Forms {
+    public partial class Comp : Form {
+        private const int TableFooterHeight = 30;
+        private const int TableLeftFrameWidth = 50;
+        private const int AmpDispUnit = 30;
+        private const int AmpDispCols = 12;
+        private const int AmpDispRows = 12;
+        private const int AmpDispWidth = AmpDispUnit * AmpDispCols;
+        private const int AmpDispHeight = AmpDispUnit * AmpDispRows;
+        private const int MinDB = -60;
+        private const int ZeroDbCol = 10;
+        private const int ZeroDbRow = 2;
+        private const int DbDispUnit = 12;
+
+        private bool mScrollThreshold = false;
+        private bool mScrollGain = false;
+
+        private double mThreshold = -9.0;
+        private double mGain = -6.0;
+
+        private CommonCtrl mCommonCtrl;
+
+        private Bitmap mBmpRow;
+        private Bitmap mBmpCol;
+        private Bitmap mBmpCell;
+        private Bitmap mBmpValue;
+        private Graphics mGRow;
+        private Graphics mGCol;
+        private Graphics mGCell;
+        private Graphics mGValue;
+
+        public Comp() {
+            InitializeComponent();
+        }
+
+        private void Comp_Load(object sender, EventArgs e) {
+            mCommonCtrl = new CommonCtrl(this);
+            drawBackground();
+            draw();
+        }
+
+        private void picCell_MouseDown(object sender, MouseEventArgs e) {
+            var pos = picCell.PointToClient(Cursor.Position);
+            if (pos.X <= AmpDispUnit * ZeroDbCol && 0 <= pos.X) {
+                mScrollThreshold = true;
+                Cursor.Current = Cursors.VSplit;
+            }
+            if (pos.X <= AmpDispWidth && AmpDispUnit * ZeroDbCol <= pos.X) {
+                mScrollGain = true;
+                Cursor.Current = Cursors.HSplit;
+            }
+        }
+
+        private void picCell_MouseUp(object sender, MouseEventArgs e) {
+            mScrollThreshold = false;
+            mScrollGain = false;
+        }
+
+        private void picCell_MouseMove(object sender, MouseEventArgs e) {
+            var pos = picCell.PointToClient(Cursor.Position);
+            var dbUnit = 5.0 * DbDispUnit / AmpDispUnit;
+            if (mScrollThreshold) {
+                mThreshold = (int)(pos.X * dbUnit + 0.5) / 10.0 + MinDB;
+                draw();
+                return;
+            }
+            if (mScrollGain) {
+                mGain = (int)((picCell.Height - pos.Y) * dbUnit + 0.5) / 10.0 + MinDB;
+                draw();
+                return;
+            }
+        }
+
+        private void draw() {
+            if (mThreshold < MinDB) {
+                mThreshold = MinDB;
+            }
+            if (0 < mThreshold) {
+                mThreshold = 0;
+            }
+
+            if (mGain < MinDB) {
+                mGain = MinDB;
+            }
+            if (0.0 < mGain) {
+                mGain = 0.0;
+            }
+            if (mGain < mThreshold) {
+                mGain = mThreshold;
+            }
+
+            var ratio = -mThreshold / (mGain - mThreshold);
+            if (0 == mThreshold) {
+                ratio = 1;
+            }
+
+            var pThresholdX = (int)((mThreshold - MinDB) * AmpDispUnit * 2 / DbDispUnit);
+            var pThresholdY = picCell.Height - pThresholdX;
+            var pGainX = (int)((mGain - MinDB) * AmpDispUnit * 2 / DbDispUnit);
+            var pGainY = picCell.Height - pGainX;
+            var pGainYT = pGainY - (int)(AmpDispUnit * (AmpDispCols - ZeroDbCol) / ratio);
+
+            var psThresholdY = pThresholdY;
+            var psGainY = pGainY;
+            if (picCell.Height < psThresholdY + 20) {
+                psThresholdY = picCell.Height - 20;
+            }
+            if (picCell.Height < psGainY + 20) {
+                psGainY = picCell.Height - 20;
+            }
+
+            if (null != picCell.Image) {
+                picCell.Image.Dispose();
+                picCell.Image = null;
+            }
+            if (null != mBmpValue) {
+                mBmpValue.Dispose();
+                mBmpValue = null;
+                mGValue.Dispose();
+                mGValue = null;
+            }
+            mBmpValue = new Bitmap(picCell.Width, picCell.Height);
+            mGValue = Graphics.FromImage(mBmpValue);
+
+            mGValue.DrawLine(Colors.PGraphLine, 0, picCell.Height, pThresholdX, pThresholdY);
+            mGValue.DrawLine(Colors.PGraphLine, pThresholdX, pThresholdY, AmpDispUnit * ZeroDbCol, pGainY);
+            mGValue.DrawLine(Colors.PGraphLineAlpha, AmpDispUnit * ZeroDbCol, pGainY, AmpDispWidth, pGainYT);
+
+            mGValue.DrawLine(Colors.PGraphLineRed, pThresholdX, 0, pThresholdX, AmpDispHeight);
+            mGValue.DrawLine(Colors.PGraphLineRed, AmpDispUnit * ZeroDbCol, pGainY, AmpDispWidth, pGainY);
+
+            mGValue.FillPie(Colors.BGraphPoint, pThresholdX - 4, pThresholdY - 4, 8, 8, 0, 360);
+            mGValue.DrawArc(Colors.PTableBorderLight, pThresholdX - 4, pThresholdY - 4, 8, 8, 0, 360);
+            mGValue.FillPie(Colors.BGraphPoint, AmpDispUnit * ZeroDbCol - 4, pGainY - 4, 8, 8, 0, 360);
+            mGValue.DrawArc(Colors.PTableBorderLight, AmpDispUnit * ZeroDbCol - 4, pGainY - 4, 8, 8, 0, 360);
+
+            mGValue.DrawString(ratio.ToString("0.0"), Fonts.Bold, Colors.BFontTable, new RectangleF(
+                AmpDispUnit * ZeroDbCol, AmpDispUnit,
+                AmpDispUnit * (AmpDispCols - ZeroDbCol), AmpDispUnit), Fonts.AlignMC);
+
+            mGValue.DrawString(mThreshold.ToString("0.0db"), Fonts.Small, Colors.BFontTable, pThresholdX + 3, psThresholdY);
+            mGValue.DrawString(mGain.ToString("0.0db"), Fonts.Small, Colors.BFontTable, AmpDispUnit * ZeroDbCol + 3, psGainY);
+
+            picCell.Image = mBmpValue;
+        }
+
+        private void drawBackground() {
+            releaseImage();
+
+            mBmpCol = new Bitmap(AmpDispWidth, TableFooterHeight);
+            mGCol = Graphics.FromImage(mBmpCol);
+            mBmpRow = new Bitmap(TableLeftFrameWidth, AmpDispHeight);
+            mGRow = Graphics.FromImage(mBmpRow);
+            mBmpCell = new Bitmap(AmpDispWidth, AmpDispHeight);
+            mGCell = Graphics.FromImage(mBmpCell);
+            //
+            // left frame
+            //
+            mGRow.Clear(Colors.TableHeader);
+            for (int row = AmpDispRows - 1, db = MinDB; 1 <= row; row -= 2, db += DbDispUnit) {
+                mGRow.DrawLine(0 == db ? Colors.PTableBorderLight : Colors.PTableBorder,
+                    0, AmpDispUnit * (row + 1),
+                    mBmpRow.Width, AmpDispUnit * (row + 1));
+                mGRow.DrawString(db.ToString("0db"), Fonts.Small, Colors.BFontTable,
+                    new RectangleF(-3, AmpDispUnit * row,
+                    mBmpRow.Width, AmpDispUnit), Fonts.AlignBR);
+            }
+            mGRow.DrawLine(Colors.PTableBorderBold,
+                TableLeftFrameWidth - 1, 0,
+                TableLeftFrameWidth - 1, mBmpRow.Height);
+            //
+            // table
+            //
+            mGCell.Clear(Colors.TableCell);
+            mGCell.FillRectangle(Colors.BTableHeader,
+                mBmpCell.Width, 0,
+                mBmpCell.Width, mBmpCell.Height);
+            for (var row = 1; row < AmpDispRows; row++) {
+                mGCell.DrawLine(0 == (row % 2) ? Colors.PTableBorder : Colors.PTableBorderDark,
+                    0, AmpDispUnit * row,
+                    mBmpCell.Width, AmpDispUnit * row);
+            }
+            for (var col = 1; col < AmpDispCols; col++) {
+                mGCell.DrawLine(0 == (col % 2) ? Colors.PTableBorder : Colors.PTableBorderDark,
+                    AmpDispUnit * col, 0,
+                    AmpDispUnit * col, mBmpCell.Height);
+            }
+            for (var y = AmpDispUnit / 2; y < AmpDispHeight; y += AmpDispUnit) {
+                for (var x = AmpDispUnit / 2; x < AmpDispWidth; x += AmpDispUnit) {
+                    mBmpCell.SetPixel(x, y, Colors.TableBorder);
+                }
+            }
+            mGCell.DrawString("Ratio", Fonts.Bold, Colors.BFontTable, new RectangleF(
+                AmpDispUnit * ZeroDbCol, 0,
+                AmpDispUnit * (AmpDispCols - ZeroDbCol), AmpDispUnit), Fonts.AlignMC);
+            mGCell.DrawLine(Colors.PTableBorderLight,
+                0, AmpDispUnit * ZeroDbRow,
+                mBmpCell.Width, AmpDispUnit * ZeroDbRow);
+            mGCell.DrawLine(Colors.PTableBorderLight,
+                AmpDispUnit * ZeroDbCol, 0,
+                AmpDispUnit * ZeroDbCol, mBmpCell.Height);
+            //
+            // footer
+            //
+            mGCol.Clear(Colors.TableHeader);
+            for (int col = 0, db = MinDB; col < AmpDispCols; col += 2, db += DbDispUnit) {
+                mGCol.DrawLine(0 == db ? Colors.PTableBorderLight : Colors.PTableBorder,
+                    AmpDispUnit * col, 0,
+                    AmpDispUnit * col, mBmpCol.Height);
+                mGCol.DrawString(db.ToString("0db"), Fonts.Small, Colors.BFontTable,
+                    new RectangleF(AmpDispUnit * col, 0,
+                    AmpDispUnit * col, mBmpCol.Height), Fonts.AlignML);
+            }
+            mGCol.DrawLine(Colors.PTableBorderBold, 0, 1, mBmpCol.Width, 1);
+
+            setImage();
+        }
+
+        private void setImage() {
+            mGRow.DrawLine(Colors.PTabBorderBold, 0, 1, mBmpRow.Width, 1);
+            mGRow.DrawLine(Colors.PTabBorderBold, 1, 0, 1, mBmpRow.Height);
+            mGRow.DrawLine(Colors.PTabBorderBold,
+                0, mBmpRow.Height - 1,
+                mBmpRow.Width, mBmpRow.Height - 1);
+            mGCol.DrawLine(Colors.PTabBorderBold, 1, 0, 1, mBmpCol.Height - 1);
+            mGCol.DrawLine(Colors.PTabBorderBold,
+                0, mBmpCol.Height - 1,
+                mBmpCol.Width - 1, mBmpCol.Height - 1);
+            mGCol.DrawLine(Colors.PTabBorderBold,
+                mBmpCol.Width - 1, 0,
+                mBmpCol.Width - 1, mBmpCol.Height);
+            mGCell.DrawLine(Colors.PTabBorderBold,
+                mBmpCell.Width - 1, 0,
+                mBmpCell.Width - 1, mBmpCell.Height);
+            mGCell.DrawLine(Colors.PTabBorderBold,
+                0, 1,
+                mBmpCell.Width - 1, 1);
+
+            Width = mBmpRow.Width + mBmpCell.Width;
+            Height = mCommonCtrl.FormCtrlBottom + mBmpCol.Height + mBmpCell.Height;
+
+            picFooter.Width = mBmpCol.Width;
+            picFooter.Height = mBmpCol.Height;
+            picRow.Width = mBmpRow.Width;
+            picRow.Height = mBmpRow.Height;
+            picCell.Width = mBmpCell.Width;
+            picCell.Height = mBmpCell.Height;
+            picFooter.BackgroundImage = mBmpCol;
+            picRow.BackgroundImage = mBmpRow;
+            picCell.BackgroundImage = mBmpCell;
+
+            picRow.Left = 0;
+            picCell.Left = picRow.Right;
+            picFooter.Left = picRow.Right;
+
+            picRow.Top = mCommonCtrl.FormCtrlBottom;
+            picCell.Top = mCommonCtrl.FormCtrlBottom;
+            picFooter.Top = picCell.Bottom;
+        }
+
+        private void releaseImage() {
+            if (null != mBmpCol) {
+                mBmpCol.Dispose();
+                mBmpCol = null;
+                mGCol.Dispose();
+                mGCol = null;
+            }
+            if (null != mBmpRow) {
+                mBmpRow.Dispose();
+                mBmpRow = null;
+                mGRow.Dispose();
+                mGRow = null;
+            }
+            if (null != mBmpCell) {
+                mBmpCell.Dispose();
+                mBmpCell = null;
+                mGCell.Dispose();
+                mGCell = null;
+            }
+
+            if (null != picFooter.BackgroundImage) {
+                picFooter.BackgroundImage.Dispose();
+                picFooter.BackgroundImage = null;
+            }
+            if (null != picRow.BackgroundImage) {
+                picRow.BackgroundImage.Dispose();
+                picRow.BackgroundImage = null;
+            }
+            if (null != picCell.BackgroundImage) {
+                picCell.BackgroundImage.Dispose();
+                picCell.BackgroundImage = null;
+            }
+        }
+    }
+}
